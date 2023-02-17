e9f13e186b14ab3142c57fd1507fcbabee382507
Iunora93 <eleonoraruggieri22@gmail.com>
2023-02-17 17:15:37 +0100

    Aggiunge 3 ristorante nel seed

--- database/seeders/RestaurantSeeder.php
+++ database/seeders/RestaurantSeeder.php
@@ -65,23 +65,25 @@ class RestaurantSeeder extends Seeder
         // Ristorante 3, Healthy
         $newRestaurant = new Restaurant;
         $newRestaurant->user_id = 3;
-        $newRestaurant->name = 'Bamboo';
+        $newRestaurant->name = 'Zumos Juicebar & healthy food';
         $newRestaurant->slug = Str::slug($newRestaurant->name, '-');
-        $newRestaurant->address = 'Piazza Filippo Andrea doria pamphili, 19';
-        $newRestaurant->description = 'Piatti americani serviti su tovagliette di carta in un piccolo locale con tavoli colorati e targhe vintage.';
-        $newRestaurant->img = 'https://res.cloudinary.com/tf-lab/image/upload/restaurant/c21ecdba-5901-4deb-9735-cd21e80ba67d/98e6373a-8de1-4f8b-9b9d-30ad382899c7.jpg';
+        $newRestaurant->address = 'Via Boncompagni, 14/D';
+        $newRestaurant->description = 'Il primo Healthy Fast Food a Roma, dove mangiare sano e bere frullati con frutta tropicale è possibile';
+        $newRestaurant->img = 'https://media-cdn.tripadvisor.com/media/photo-s/1a/ee/a3/b9/don-t-worry-be-healthy.jpg';
         $newRestaurant->vat = $faker->vat();
-        $newRestaurant->opening_time = '07:00:00';
+        $newRestaurant->opening_time = '11:30:00';
         $newRestaurant->closing_time = '22:30:00';
         $newRestaurant->phone_number = $faker->phoneNumber();
         $newRestaurant->price_shipping = $faker->randomFloat(2, 1, 4);
         $newRestaurant->save();
         $newRestaurant->categories()->attach(1);
+        $newRestaurant->categories()->attach(3);
+        $newRestaurant->categories()->attach(6);
 
 
         // Ristorante 4, Indiano
         $newRestaurant = new Restaurant;
-        $newRestaurant->user_id = 2;
+        $newRestaurant->user_id = 4;
         $newRestaurant->name = 'Bamboo';
         $newRestaurant->slug = Str::slug($newRestaurant->name, '-');
         $newRestaurant->address = 'Piazza Filippo Andrea doria pamphili, 19';
