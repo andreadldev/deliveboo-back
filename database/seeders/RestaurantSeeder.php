7f010aa745cce1ead6bdb1177d8a82e8a14a6cbf
Iunora93 <eleonoraruggieri22@gmail.com>
2023-02-17 17:46:05 +0100

    Termina ristoranti seeder

--- database/seeders/RestaurantSeeder.php
+++ database/seeders/RestaurantSeeder.php
@@ -144,11 +144,11 @@ class RestaurantSeeder extends Seeder
         // Ristorante 7, Messicano
         $newRestaurant = new Restaurant;
         $newRestaurant->user_id = 7;
-        $newRestaurant->name = 'Bamboo';
+        $newRestaurant->name = 'La Cucaracha';
         $newRestaurant->slug = Str::slug($newRestaurant->name, '-');
-        $newRestaurant->address = 'Piazza Filippo Andrea doria pamphili, 19';
-        $newRestaurant->description = 'Piatti americani serviti su tovagliette di carta in un piccolo locale con tavoli colorati e targhe vintage.';
-        $newRestaurant->img = 'https://res.cloudinary.com/tf-lab/image/upload/restaurant/c21ecdba-5901-4deb-9735-cd21e80ba67d/98e6373a-8de1-4f8b-9b9d-30ad382899c7.jpg';
+        $newRestaurant->address = 'Via Mocenigo, 10';
+        $newRestaurant->description = 'Tacos, quesadillas e fajitas, oltre a cocktail a base di tequila, in un locale accogliente con colorati murales messicani.';
+        $newRestaurant->img = 'https://media-cdn.tripadvisor.com/media/photo-s/10/bd/7d/bb/fajitas-de-pollo-chiken.jpg';
         $newRestaurant->vat = $faker->vat();
         $newRestaurant->opening_time = '19:00:00';
         $newRestaurant->closing_time = '23:30:00';
@@ -161,69 +161,85 @@ class RestaurantSeeder extends Seeder
 
         // Ristorante 8, Poke
         $newRestaurant = new Restaurant;
-        $newRestaurant->user_id = 2;
-        $newRestaurant->name = 'Bamboo';
+        $newRestaurant->user_id = 8;
+        $newRestaurant->name = 'Gusto Poke';
         $newRestaurant->slug = Str::slug($newRestaurant->name, '-');
-        $newRestaurant->address = 'Piazza Filippo Andrea doria pamphili, 19';
-        $newRestaurant->description = 'Piatti americani serviti su tovagliette di carta in un piccolo locale con tavoli colorati e targhe vintage.';
-        $newRestaurant->img = 'https://res.cloudinary.com/tf-lab/image/upload/restaurant/c21ecdba-5901-4deb-9735-cd21e80ba67d/98e6373a-8de1-4f8b-9b9d-30ad382899c7.jpg';
+        $newRestaurant->address = 'Viale Leonardo da Vinci, 264';
+        $newRestaurant->description = 'Elegante wine bar in stile industrial chic, con mattoni a vista, ampie vetrate e menù su lavagna.';
+        $newRestaurant->img = 'https://media-cdn.tripadvisor.com/media/photo-s/1b/f8/2b/2d/variedad-de-poke-bowls.jpg';
         $newRestaurant->vat = $faker->vat();
-        $newRestaurant->opening_time = '07:00:00';
+        $newRestaurant->opening_time = '11:00:00';
         $newRestaurant->closing_time = '22:30:00';
         $newRestaurant->phone_number = $faker->phoneNumber();
         $newRestaurant->price_shipping = $faker->randomFloat(2, 1, 4);
         $newRestaurant->save();
-        $newRestaurant->categories()->attach(1);
+        $newRestaurant->categories()->attach(2);
+        $newRestaurant->categories()->attach(3);
+        $newRestaurant->categories()->attach(8);
+        $newRestaurant->categories()->attach(10);
+
+
+
 
 
         // Ristorante 9, FastFood
         $newRestaurant = new Restaurant;
-        $newRestaurant->user_id = 2;
-        $newRestaurant->name = 'Bamboo';
+        $newRestaurant->user_id = 9;
+        $newRestaurant->name = 'Birretta wine and food - Hamburger';
         $newRestaurant->slug = Str::slug($newRestaurant->name, '-');
-        $newRestaurant->address = 'Piazza Filippo Andrea doria pamphili, 19';
-        $newRestaurant->description = 'Piatti americani serviti su tovagliette di carta in un piccolo locale con tavoli colorati e targhe vintage.';
-        $newRestaurant->img = 'https://res.cloudinary.com/tf-lab/image/upload/restaurant/c21ecdba-5901-4deb-9735-cd21e80ba67d/98e6373a-8de1-4f8b-9b9d-30ad382899c7.jpg';
+        $newRestaurant->address = 'Via Leopoldo Nobili, 36';
+        $newRestaurant->description = 'Burger bar in stile industrial-chic che serve piatti vegani, carne e pollo locali accompagnati da birre artigianali.';
+        $newRestaurant->img = 'http://www.studioristorante.com/wp-content/uploads/2017/07/5630be0a8fc825d5ac1c5575.jpg';
         $newRestaurant->vat = $faker->vat();
         $newRestaurant->opening_time = '07:00:00';
-        $newRestaurant->closing_time = '22:30:00';
+        $newRestaurant->closing_time = '23:30:00';
         $newRestaurant->phone_number = $faker->phoneNumber();
         $newRestaurant->price_shipping = $faker->randomFloat(2, 1, 4);
         $newRestaurant->save();
         $newRestaurant->categories()->attach(1);
+        $newRestaurant->categories()->attach(9);
+        $newRestaurant->categories()->attach(11);
+
 
 
         // Ristorante 10, Sushi
         $newRestaurant = new Restaurant;
-        $newRestaurant->user_id = 2;
-        $newRestaurant->name = 'Bamboo';
+        $newRestaurant->user_id = 10;
+        $newRestaurant->name = 'Neko Sushi Monteverde';
         $newRestaurant->slug = Str::slug($newRestaurant->name, '-');
-        $newRestaurant->address = 'Piazza Filippo Andrea doria pamphili, 19';
-        $newRestaurant->description = 'Piatti americani serviti su tovagliette di carta in un piccolo locale con tavoli colorati e targhe vintage.';
-        $newRestaurant->img = 'https://res.cloudinary.com/tf-lab/image/upload/restaurant/c21ecdba-5901-4deb-9735-cd21e80ba67d/98e6373a-8de1-4f8b-9b9d-30ad382899c7.jpg';
+        $newRestaurant->address = 'Piazzale Eugenio Morelli, 14';
+        $newRestaurant->description = 'Riso alla cantonese e sushi, anche nella formula all you can eat, in ristorante con arredi scuri essenziali.';
+        $newRestaurant->img = 'https://www.cosafarearoma.it/wp-content/uploads/2018/10/Migliore-Sushi-a-Roma-Ristoranti-Giapponesi.jpg';
         $newRestaurant->vat = $faker->vat();
-        $newRestaurant->opening_time = '07:00:00';
+        $newRestaurant->opening_time = '19:00:00';
         $newRestaurant->closing_time = '22:30:00';
         $newRestaurant->phone_number = $faker->phoneNumber();
         $newRestaurant->price_shipping = $faker->randomFloat(2, 1, 4);
         $newRestaurant->save();
-        $newRestaurant->categories()->attach(1);
+        $newRestaurant->categories()->attach(2);
+        $newRestaurant->categories()->attach(3);
+        $newRestaurant->categories()->attach(8);
+        $newRestaurant->categories()->attach(10);
+        $newRestaurant->categories()->attach(11);
 
 
         // Ristorante 11, Dolci
         $newRestaurant = new Restaurant;
-        $newRestaurant->user_id = 2;
-        $newRestaurant->name = 'Bamboo';
+        $newRestaurant->user_id = 11;
+        $newRestaurant->name = 'I Dolci Di Checco Er Carettiere';
         $newRestaurant->slug = Str::slug($newRestaurant->name, '-');
-        $newRestaurant->address = 'Piazza Filippo Andrea doria pamphili, 19';
-        $newRestaurant->description = 'Piatti americani serviti su tovagliette di carta in un piccolo locale con tavoli colorati e targhe vintage.';
-        $newRestaurant->img = 'https://res.cloudinary.com/tf-lab/image/upload/restaurant/c21ecdba-5901-4deb-9735-cd21e80ba67d/98e6373a-8de1-4f8b-9b9d-30ad382899c7.jpg';
+        $newRestaurant->address = 'Via Benedetta, 7';
+        $newRestaurant->description = 'Elegante caffè/bar per colazione e pranzi leggeri, oltre a caffè, dolci artigianali e gelati.';
+        $newRestaurant->img = 'https://www.bajaroma.it/wp-content/uploads/2020/04/Baja-Ristorante-Dolce.jpg';
         $newRestaurant->vat = $faker->vat();
-        $newRestaurant->opening_time = '07:00:00';
-        $newRestaurant->closing_time = '22:30:00';
+        $newRestaurant->opening_time = '06:00:00';
+        $newRestaurant->closing_time = '15:30:00';
         $newRestaurant->phone_number = $faker->phoneNumber();
         $newRestaurant->price_shipping = $faker->randomFloat(2, 1, 4);
         $newRestaurant->save();
         $newRestaurant->categories()->attach(1);
+        $newRestaurant->categories()->attach(5);
+        $newRestaurant->categories()->attach(6);
+        $newRestaurant->categories()->attach(11);
     }
 }
