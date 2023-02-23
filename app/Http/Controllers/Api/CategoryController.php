a25116bddf0e8434e6e17fff361b0277e3bdc911
32Fire32 <nicola.gianfelici@hotmail.it>
2023-02-23 17:27:21 +0100

    change api category

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -9,7 +9,7 @@ use App\Models\Category;
 class CategoryController extends Controller
 {
     public function index(){
-        $categories = Category::all();
+        $categories = Category::has('projects')->with('projects')->get();
 
         return $categories;
     }
