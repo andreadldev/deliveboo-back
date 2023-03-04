9606a16b7d641c6093b76d62b56db0af436228b9
32Fire32 <nicola.gianfelici@hotmail.it>
2023-03-04 14:10:49 +0100

    rifiniture

--- resources/views/admin/dishes/create.blade.php
+++ resources/views/admin/dishes/create.blade.php
@@ -87,7 +87,7 @@
                     </div>
                     <div class="mb-4 row mb-0">
                         <div class="col-md-6 offset-md-4">
-                            <button type="submit" class="btn btn-primary">
+                            <button type="submit" class="btn btn-warning">
                                 {{ __('Inserisci') }}
                             </button>
                         </div>
